46aeb9bafff70ce36b8b64cc8e1b34b7143d1f66
Manoel Silva <manoelmarcelomms@gmail.com>
2025-06-08 19:53:43 -0300

    added color again

--- mandlebrot.c
+++ mandlebrot.c
@@ -1,4 +1,3 @@
-/* Sequential Mandlebrot program */
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
@@ -8,13 +7,54 @@
 #include <math.h>
 #include <omp.h>
 
-#define X_RESN 1024       /* x resolution */
-#define Y_RESN 1024       /* y resolution */
+#define X_RESN 1200      /* x resolution */
+#define Y_RESN 1200      /* y resolution */
+#define NUM_COLORS 8     /* número de cores na paleta */
 
 typedef struct complextype {
     float real, imag;
 } Compl;
 
+/* Paleta de cores global */
+unsigned long color_palette[NUM_COLORS];
+
+/* Inicializar paleta de cores */
+void init_color_palette(Display *display, int screen) {
+    XColor color;
+    Colormap colormap = DefaultColormap(display, screen);
+    
+    // Definir cores da paleta (RGB em 16-bit: 0-65535)
+    unsigned short palette_rgb[NUM_COLORS][3] = {
+        {0,     0,     0},      // Preto
+        {65535, 0,     0},      // Vermelho
+        {0,     65535, 0},      // Verde
+        {0,     0,     65535},  // Azul
+        {65535, 65535, 0},      // Amarelo
+        {65535, 0,     65535},  // Magenta
+        {0,     65535, 65535},  // Ciano
+        {65535, 32767, 0}       // Laranja
+    };
+    
+    for (int i = 0; i < NUM_COLORS; i++) {
+        color.red = palette_rgb[i][0];
+        color.green = palette_rgb[i][1];
+        color.blue = palette_rgb[i][2];
+        color.flags = DoRed | DoGreen | DoBlue;
+        
+        if (XAllocColor(display, colormap, &color)) {
+            color_palette[i] = color.pixel;
+        } else {
+            color_palette[i] = BlackPixel(display, screen);
+        }
+    }
+}
+
+/* Função para obter cor da paleta baseada na posição */
+unsigned long get_color_from_palette(int i, int j) {
+    // Usar coordenadas para selecionar cor da paleta
+    int color_index = (i / 100 + j / 100) % NUM_COLORS;
+    return color_palette[color_index];
+}
 
 void main() {
     Window      win;                            /* initialization for a window */
@@ -25,7 +65,7 @@ void main() {
                 display_width, display_height,  /* size of screen */
                 screen;                         /* which screen */
 
-    char        *window_name = "Mandelbrot Set", *display_name = NULL;
+    char        *window_name = "Mandelbrot Set Colorido", *display_name = NULL;
     GC          gc;
     unsigned
     long        valuemask = 0;
@@ -33,7 +73,7 @@ void main() {
     Display     *display;
     XSizeHints	size_hints;
     Pixmap      bitmap;
-    XPoint      points[800];
+    XPoint      points[1200];
     FILE        *fp, *fopen();
     char        str[100];
     
@@ -57,8 +97,8 @@ void main() {
     display_height = DisplayHeight(display, screen);
 
     /* set window size */
-    width = 1024;
-    height = 1024;
+    width = 1200;
+    height = 1200;
 
     /* set window position */
     x = 0;
@@ -97,15 +137,18 @@ void main() {
     XMapWindow(display, win);
     XSync(display, 0);
 
+    /* Inicializar paleta de cores */
+    init_color_palette(display, screen);
+
     double t_inicio = omp_get_wtime();
            
     /* Calculate and draw points */
-    #pragma omp parallel for default(none) private(j, z, c, k, temp, lengthsq) shared(display, win, gc) schedule(static)
+    #pragma omp parallel for default(none) private(j, z, c, k, temp, lengthsq) shared(display, win, gc, screen) schedule(static)
     for (i = 0; i < X_RESN; i++) {
         for (j = 0; j < Y_RESN; j++) {
             z.real = z.imag = 0.0;
-            c.real = ((float) j - 400.0)/200.0;            /* scale factors for 800 x 800 window */
-            c.imag = ((float) i - 400.0)/200.0;
+            c.real = ((float) j - 600.0)/300.0;            /* scale factors for 1200 x 1200 window */
+            c.imag = ((float) i - 600.0)/300.0;
             k = 0;
 
             do {                                           /* iterate for pixel color */
@@ -119,6 +162,8 @@ void main() {
             if (k == 100) {
                 #pragma omp critical
                 {
+                    unsigned long color = get_color_from_palette(i, j);
+                    XSetForeground(display, gc, color);
                     XDrawPoint(display, win, gc, j, i);
                 }                
             }
