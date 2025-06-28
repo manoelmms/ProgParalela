9f6950cfe2c52b6239f016d840b93c0c4be82165
manoelmms <manoelmarcelomms@gmail.com>
2025-06-28 20:25:09 -0300

    Fix strassen

--- Trabalho 3/compile.sh
+++ Trabalho 3/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
-# Compile the naive matrix multiplication program using intel compiler
-icc -qopenmp -o naive_mul naive_mul.c -lm
+# Compile the naive matrix multiplication program using gcc
+gcc -O1 -fopenmp -o naive_mul naive_mul.c -lm
 # Compile the optimized matrix multiplication program using intel compiler
-icc -O3 -qopenmp -o optimized_mul optimized_mul.c -lm
+gcc -DL1D_CACHE_TAM=$(getconf LEVEL1_DCACHE_LINESIZE) -O3 -fno-tree-loop-vectorize -fno-tree-slp-vectorize -fno-tree-vectorize -fopenmp -o optimized_mul optimized_mul.c -lm
 # Compile the Strassen algorithm program using intel compiler
-icc -O3 -qopenmp -o strassen_omp_tsk strassen_omp_tsk.c -lm
+gcc -DL1D_CACHE_TAM=$(getconf LEVEL1_DCACHE_LINESIZE) -O3 -fno-tree-loop-vectorize -fno-tree-slp-vectorize -fno-tree-vectorize -fopenmp -o strassen_omp_tsk strassen_omp_tsk.c -lm
 # Compile the Strassen algorithm program using intel compiler vectorized MIC-AVX512
-icc -O3 -qopenmp -xMIC-AVX512 -o strassen_omp_tsk_mic strassen_omp_tsk.c -lm
+gcc -DL1D_CACHE_TAM=$(getconf LEVEL1_DCACHE_LINESIZE) -O3 -mavx512f -mavx512er -mavx512cd -mavx512pf -fopenmp -o strassen_omp_tsk_mic strassen_omp_tsk.c -lm
